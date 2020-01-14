68f851846d93a754d6b83603248e2c8220ee57cb
Michael Wilkes <mhwilkes@gmail.com>
2020-01-14 12:25:04 -0500

    initial commit

---
+++ app/src/main/java/emu/dev/spotify_swipe/MainActivity.kt
@@ -0,0 +1,12 @@
+package emu.dev.spotify_swipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
