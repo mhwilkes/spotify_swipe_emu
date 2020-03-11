d5383c38c3d82f8aa9bca7bf107bf55a757362e3
Mike <mhwilkes@gmail.com>
2020-03-11 10:28:47 -0400

    Follow API function params

--- app/src/main/java/emu/dev/spotify_swipe/api/data/Cursor.kt
+++ app/src/main/java/emu/dev/spotify_swipe/api/data/Cursor.kt
@@ -1,6 +1,14 @@
 package emu.dev.spotify_swipe.api.data
 
 data class Cursor(
-    val text: String?,
-    val type: String
+    val after: String
+)
+
+data class CursorPage<T>(
+    val href: String,
+    val items: Array<T>,
+    val limit: Int,
+    val next: String,
+    val cursors: Cursor,
+    val total: Int
 )
--- app/src/main/java/emu/dev/spotify_swipe/api/endpoints/FollowAPI.kt
+++ app/src/main/java/emu/dev/spotify_swipe/api/endpoints/FollowAPI.kt
@@ -1,12 +1,15 @@
 package emu.dev.spotify_swipe.api.endpoints
 
+import emu.dev.spotify_swipe.api.data.Artist
+import emu.dev.spotify_swipe.api.data.CursorPage
 import emu.dev.spotify_swipe.api.spotify.SpotifyRequest
 
 class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     // @GET
     // Requires scope: USER_FOLLOW_READ("user-follow-read")
     suspend fun checkFollowUserArtist(
-
+        type: String,
+        vararg ids: String
     ): Boolean {
 
     }
@@ -14,7 +17,8 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     // @GET
     // Requires scope: PLAYLIST_READ_PRIVATE("playlist-read-private")
     suspend fun checkFollowPlaylist(
-
+        playlist_id: String,
+        vararg ids: String
     ): Boolean {
 
     }
@@ -22,7 +26,8 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     // @PUT
     // Requires scope: USER_FOLLOW_MODIFY("user-follow_modify")
     suspend fun followArtistUser(
-
+        type: String,
+        vararg ids: String
     ) {
 
     }
@@ -31,7 +36,8 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     // Scope required: PLAYLIST_MODIFY_PUBLIC("playlist-modify-public")
     // Scope required: PLAYLIST_MODIFY_PRIVATE("playlist-modify-private")
     suspend fun followPlaylist(
-
+        playlist_id: String,
+        public: Boolean = true
     ) {
 
     }
@@ -39,15 +45,18 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     // @GET
     // Requires scope: USER_FOLLOW_READ("user-follow-read")
     suspend fun getFollowedArtists(
-
-    ) {
+        type: String,
+        limit: Int = 20,
+        after: String? = null
+    ): CursorPage<Artist> {
 
     }
 
     // @DELETE
     // Requires scope: USER_FOLLOW_MODIFY("user-follow_modify")
     suspend fun unfollowArtistUser(
-
+        type: String,
+        vararg ids: String
     ) {
 
     }
@@ -56,7 +65,7 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     // Scope required: PLAYLIST_MODIFY_PUBLIC("playlist-modify-public")
     // Scope required: PLAYLIST_MODIFY_PRIVATE("playlist-modify-private")
     suspend fun unfollowPlaylist(
-
+        playlist_id: String
     ) {
 
     }
