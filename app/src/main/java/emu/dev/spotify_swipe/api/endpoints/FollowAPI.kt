76a673d2ae02c6086111bac27b2f82dd1156ed00
Mike <mhwilkes@gmail.com>
2020-03-14 12:00:05 -0400

    Follow API requests

--- app/src/main/java/emu/dev/spotify_swipe/api/endpoints/FollowAPI.kt
+++ app/src/main/java/emu/dev/spotify_swipe/api/endpoints/FollowAPI.kt
@@ -4,18 +4,15 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import emu.dev.spotify_swipe.api.data.Artist
 import emu.dev.spotify_swipe.api.data.CursorPage
-import emu.dev.spotify_swipe.api.data.Recommendation
 import emu.dev.spotify_swipe.api.spotify.SpotifyRequest
-import io.ktor.client.request.accept
-import io.ktor.client.request.get
-import io.ktor.client.request.header
+import io.ktor.client.request.*
 import io.ktor.http.ContentType
 
 class FollowAPI(private val spotifyRequest: SpotifyRequest) {
 
     val FOLLOW_ENDPOINT =
-    spotifyRequest.DEFAULT_ENDPOINT
-        .plus("/me/following/")
+        spotifyRequest.DEFAULT_ENDPOINT
+            .plus("/me/following/")
 
 
     // @GET
@@ -68,7 +65,7 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     ) {
         val typeToken = object : TypeToken<Boolean>() {}.type
         val response =
-            spotifyRequest.client.get<String>(
+            spotifyRequest.client.put<String>(
                 FOLLOW_ENDPOINT
                     .plus("?type=$type")
                     .plus("&ids=${ids.joinToString(separator = "%2C")}")
@@ -87,7 +84,15 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
         playlist_id: String,
         public: Boolean = true
     ) {
-
+        val response =
+            spotifyRequest.client.put<String>(
+                spotifyRequest.DEFAULT_ENDPOINT
+                    .plus("/playlists/$playlist_id/followers")
+                    .plus("?public=$public")
+            ) {
+                accept(ContentType.Application.Json)
+                header("Authorization", "Bearer ${spotifyRequest.token.access_token}")
+            }
     }
 
     // @GET
@@ -97,7 +102,19 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
         limit: Int = 20,
         after: String? = null
     ): CursorPage<Artist> {
+        val typeToken = object : TypeToken<CursorPage<Artist>>() {}.type
+        val response =
+            spotifyRequest.client.get<String>(
+                FOLLOW_ENDPOINT
+                    .plus("?type=$type")
+                    .plus("&limit=$limit")
+                    .plus(if (after != null) "&after=$after" else "")
+            ) {
+                accept(ContentType.Application.Json)
+                header("Authorization", "Bearer ${spotifyRequest.token.access_token}")
+            }
 
+        return Gson().fromJson(response, typeToken)
     }
 
     // @DELETE
@@ -106,7 +123,15 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
         type: String,
         vararg ids: String
     ) {
-
+        val response =
+            spotifyRequest.client.delete<String>(
+                FOLLOW_ENDPOINT
+                    .plus("?type=$type")
+                    .plus("&ids=${ids.joinToString(separator = "%2C")}")
+            ) {
+                accept(ContentType.Application.Json)
+                header("Authorization", "Bearer ${spotifyRequest.token.access_token}")
+            }
     }
 
     // @DELETE
@@ -115,6 +140,12 @@ class FollowAPI(private val spotifyRequest: SpotifyRequest) {
     suspend fun unfollowPlaylist(
         playlist_id: String
     ) {
-
+        spotifyRequest.client.delete<String>(
+            spotifyRequest.DEFAULT_ENDPOINT
+                .plus("/playlists/$playlist_id/followers")
+        ) {
+            accept(ContentType.Application.Json)
+            header("Authorization", "Bearer ${spotifyRequest.token.access_token}")
+        }
     }
 }
