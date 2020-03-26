76d86ebd4239cd774f9ac8e5736b2eb2c8f37e47
Mike <mhwilkes@gmail.com>
2020-03-26 15:27:07 -0400

    tutorial card activity and recommendation activity for user flow

---
+++ app/src/main/java/emu/dev/spotify_swipe/RecommendationActivity.kt
@@ -0,0 +1,11 @@
+package emu.dev.spotify_swipe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecommendationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/emu/dev/spotify_swipe/TutorialActivity.kt
@@ -0,0 +1,16 @@
+package emu.dev.spotify_swipe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yuyakaido.android.cardstackview.CardStackListener
+
+
+class TutorialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
